4702c38ee0a3a4803dca4db5ba43294db3b0ea20
paconte <paconte@gmail.com>
2017-07-15 11:55:10 +0200

    If current date is weekend then available dates are all the week working days.

--- src/state.js
+++ src/state.js
@@ -2,18 +2,26 @@ import products from './test';
 
 const moment = require('moment');
 
+
 const currentDate = moment();
 const momentFormat = 'dddd - DD.MM.YYYY';
 
+
 function formatDate(date) { return date.format(momentFormat); }
+
 
 function getAvailableDates() {
   const result = [];
   let day;
+  const saturday = moment().day(6);
+  const sunday = moment().day(0);
   // Sunday=0 and Saturday=6
   for (let i = 1; i < 6; i += 1) {
     day = moment().day(i);
-    if (day.isSameOrAfter(currentDate, 'day')) { result.push(day); }
+    if (day.isSameOrAfter(currentDate, 'day')
+      || currentDate.isSame(saturday, 'day') || currentDate.isSame(sunday, 'day')) {
+      result.push(day);
+    }
   }
   return result;
 }
