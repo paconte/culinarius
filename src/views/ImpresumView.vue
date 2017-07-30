e58f89472a990c955e2f476c6fbc918f73777dff
paconte <paconte@gmail.com>
2017-07-30 17:13:13 +0200

    Update Impressum.

--- src/views/ImpresumView.vue
+++ src/views/ImpresumView.vue
@@ -12,19 +12,13 @@
       <div class="container">
         <h1 class="subtitle">Verantwortlich für dieses Angebot gem&auml;&szlig; &sect; § 5 TMG / § 55 RStV</h1>
         <p>
-          paconte<br/>
+          Francisco Javier Revilla Linares<br/>
           Leuthener Stra&szlig;e 9<br/>
-          10839 Berlin<br/><br/>
+          10829 Berlin<br/><br/>
         </p>
-        <h1 class="subtitle">Pers&ouml;nlich haftende Gesellschafter</h1>
-        <p>
-          Francisco Javier Revilla Linares<br/><br/>
-        </p>
-        <h1 class="subtitle">Kontakt</h1>
         <p>
           Telefon: 017663622753<br/>
-          E-Mail: <a href="mailto:paconte@gmail.com">paconte@gmail.com</a><br/>
-          Internetadresse: <a href="http://www.paconte.com" target="_blank">www.paconte.com</a>
+          E-Mail: <a href="mailto:franciscojavier.revilla@gmail.com">franciscojavier.revilla (at) gmail.com</a><br/>
         </p>
       </div>
     </section>
