4d043c734ca4c03f53c7ab331920f0c906639030
paconte <paconte@gmail.com>
2017-08-10 20:56:13 +0200

    Add google analytics. Npm package and configuration.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
 import App from './App';
 import router from './router';
 
+
 Vue.config.productionTip = false;
+
+Vue.use(VueAnalytics, {
+  id: 'UA-83525222-4',
+  router,
+  autoTracking: {
+    pageviewOnLoad: false,
+  },
+});
 
 /* eslint-disable no-new */
 new Vue({
