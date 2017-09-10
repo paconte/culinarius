33ee4b9924ac365ad88cab771431abbbfb6373fc
paconte <paconte@gmail.com>
2017-09-10 17:35:09 +0200

    Use coolinarius user for deploying instead of root.

--- prepare_deploy_script.sh
+++ prepare_deploy_script.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 # variables
-PUBLISH_HOST=root@frevilla.com
+USER_HOST=coolinarius
+PUBLISH_HOST=${USER_HOST}@frevilla.com
+FILES_HOST=/home/${USER_HOST}/
 DEPLOY_SCRIPT=server_deploy.sh
 
 # delete files
@@ -18,9 +20,9 @@ zip -r dist.zip ./dist/
 
 # copy files to the server
 echo "Copy files to host."
-scp ./dist.zip ./${DEPLOY_SCRIPT} ${PUBLISH_HOST}:/root/
+scp ./dist.zip ./${DEPLOY_SCRIPT} ${PUBLISH_HOST}:${FILES_HOST}
 
 # deploy for production
 echo "Deploy for prodution."
-ssh ${PUBLISH_HOST} sh /root/${DEPLOY_SCRIPT}
-ssh ${PUBLISH_HOST} rm /root/${DEPLOY_SCRIPT}
+ssh ${PUBLISH_HOST} sh ${FILES_HOST}${DEPLOY_SCRIPT}
+ssh ${PUBLISH_HOST} rm ${FILES_HOST}${DEPLOY_SCRIPT}
