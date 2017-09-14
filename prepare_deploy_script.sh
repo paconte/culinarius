2dfbc6cb8bd3e8e2d8ea3333fa680aadddee78ab
paconte <paconte@gmail.com>
2017-09-15 01:03:25 +0200

    scp and ssh command with specific private_key.

--- prepare_deploy_script.sh
+++ prepare_deploy_script.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e
+set -x
 
 # variables
 LOCAL_PATH=$( cd $(dirname $0) ; pwd -P ) #/home/frevilla/devel/culinarius/
@@ -6,25 +8,31 @@ USER_HOST=coolinarius
 PUBLISH_HOST=${USER_HOST}@frevilla.com
 FILES_HOST=/home/${USER_HOST}/
 DEPLOY_SCRIPT=server_deploy.sh
+KEY=/home/frevilla/.ssh/id_rsa_culinarius
+ABC=/home/frevilla/devel/scrapy/mahlzeit/deploy_timestamps.txt
 
 # delete files
-set +e
 cd ${LOCAL_PATH}
+set +e
 echo "Deleting local files."
 rm ./dist/*.*
 rm ./dist.zip
+set -e
 
 # build
-set -e
 echo "Building for production."
 npm run build
+#/usr/local/lib/node_modules/npm/bin/npm-cli.js run build
+echo "Zip production files." >> ${ABC}
+echo $(date +%Y-%m-%d:%H:%M:%S) >> ${ABC}
 zip -r dist.zip ./dist/
 
 # copy files to the server
-echo "Copy files to host."
-scp ./dist.zip ./${DEPLOY_SCRIPT} ${PUBLISH_HOST}:${FILES_HOST}
+echo "SCopy files to host."
+scp -i ${KEY} ./dist.zip ./${DEPLOY_SCRIPT} ${PUBLISH_HOST}:${FILES_HOST}
 
 # deploy for production
 echo "Deploy for prodution."
-ssh ${PUBLISH_HOST} sh ${FILES_HOST}${DEPLOY_SCRIPT}
-ssh ${PUBLISH_HOST} rm ${FILES_HOST}${DEPLOY_SCRIPT}
+ssh -i ${KEY} ${PUBLISH_HOST} sh ${FILES_HOST}${DEPLOY_SCRIPT}
+ssh -i ${KEY} ${PUBLISH_HOST} rm ${FILES_HOST}${DEPLOY_SCRIPT}
+echo "Production deploy finished."
